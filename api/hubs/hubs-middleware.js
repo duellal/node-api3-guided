a5a226d993848565be644d5ad049ba7d73541d14
duellal <alexandriaduell@gmail.com>
2022-12-06 15:31:07 -0500

    Complete

---
+++ api/hubs/hubs-middleware.js
@@ -0,0 +1,42 @@
+//
+
+const Hubs = require('./hubs-model.js');
+
+async function checkHubId(req, res, next){
+   try{
+     const hub = await Hubs.findById(req.params.id)
+
+     if(hub){
+      req.hub = hub
+      next()
+     }
+     else{
+      next({status: 404, message: `Hub ${req.params.id} not found`})
+     }
+   }
+   catch(err){
+      next(err)
+   }
+}
+
+function checkNewHub(req, res, next){
+   const {name} = req.body
+
+   if(name !== undefined && 
+      //Below '    ' will get past this validation
+      typeof name === 'string' && 
+      name.length &&
+      //Below will avoid the '    ' from getting past validation
+      name.trim().length){
+      next()
+   }
+   else{
+      next({status: 422, 
+         message: `hubs need a name`})
+   }
+}
+
+module.exports = {
+   checkHubId,
+   checkNewHub,  
+}
--- api/server.js
+++ api/server.js
@@ -1,16 +1,39 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require('morgan')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//Middleware:
+function customMorgan(req, res, next){
+  console.log(`You made a ${req.method} request`)
+  next()
+}
+
+//B/c the next() was not invoked, the request kept looping
+function shortCircuit(req, res, next){
+  res.json(`The request was short cicuited`)
+}
+
+//Will not show in the console or the http request - will show in httpie if you interpolate it in the server.get() function
+function addFriend(req, res, next){
+  req.friend = `Breaking Benjamin`
+  next()
+}
+
 server.use(express.json());
+server.use(morgan('dev'))
+//invoking middleware:
+server.use(customMorgan)
+// server.use(shortCircuit)
+server.use(addFriend)
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Hubs API</h2>
+    <h2>Hubs API ${req.friend}</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
